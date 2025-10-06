6ed4ba4df433e65b0940d1ab42795efb9c8f97ba
zulqarnain87 <zulqarnainhasni2002@gmail.com>
2025-10-07 03:37:56 +0500

    ok

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,28 +1,24 @@
-// src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
-import ChatList from "./pages/ChatList";
-import ChatWindow from "./pages/ChatWindow";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import PrivateRoute from "./components/PrivateRoute"; 
+// src/App.jsx 
+// 💡 Note the change from BrowserRouter to HashRouter
+import { HashRouter, Routes, Route } from "react-router-dom"; 
+// ... other imports
 
 function App() {
   return (
-    // 💡 FIX: Add the basename property set to your repository name
-    <BrowserRouter basename="/ChatApp"> 
+    // 💡 Use HashRouter instead of BrowserRouter
+    <HashRouter> 
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
 
         {/* Protected area */}
-        {/* These routes are now relative to /ChatApp */}
+        {/* The URL will now look like: /ChatApp/#/discover */}
         <Route path="/" element={<PrivateRoute><ChatList /></PrivateRoute>} />
         <Route path="/discover" element={<PrivateRoute><Discover /></PrivateRoute>} />
         <Route path="/chats" element={<PrivateRoute><ChatList /></PrivateRoute>} />
         <Route path="/chat/:chatId" element={<PrivateRoute><ChatWindow /></PrivateRoute>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
